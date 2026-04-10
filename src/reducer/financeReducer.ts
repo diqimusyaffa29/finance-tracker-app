55960b814af84860621bb9ce24741d9d8e43b8fa
diqimusyaffa29 <diqimusyaffa4@gmail.com>
2026-04-10 20:36:39 +0800

    feat: :sparkles: Added finance reducer

---
+++ src/reducer/financeReducer.ts
@@ -0,0 +1,35 @@
+import type { FinanceAction, FinanceState } from "../types/finance.types";
+
+const now = new Date()
+
+export const initialState: FinanceState = {
+    transactions: [],
+    budgets:[],
+    filter: {
+        year: now.getFullYear(),
+        month: now.getMonth(),
+        category: "all"
+    },
+}
+
+export function financeReducer(state: FinanceState, action: FinanceAction) : FinanceState{
+    switch (action.type) {
+        case "ADD_TRANSACTION":    
+            return {
+                ...state,
+                transactions: [action.payload, ...state.transactions]
+            }
+        case "DELETE_TRANSACTION":    
+            return {
+                ...state,
+                transactions: state.transactions.filter(t => t.id !== action.payload)
+            }
+        case "SET_FILTER":   
+            return {
+                ...state,
+                filter: {...state.filter, ...action.payload}
+            }
+        default:
+            return state;
+    }
+}
