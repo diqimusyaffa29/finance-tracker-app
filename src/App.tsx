1013d330af1b1ec73cd25654c57d22c9de23982f
diqimusyaffa29 <diqimusyaffa4@gmail.com>
2026-04-10 21:44:31 +0800

    feat: :sparkles: Integrated FinanceProvider to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { BrowserRouter } from "react-router-dom"
+import { FinanceProvider } from "./context/FinanceContext"
+
 function App() {
 
   return (
-    <div className="">
-      <h1 className="text-red-400">Kocak gaming</h1>
-    </div>
+    <FinanceProvider>
+      <BrowserRouter>
+        <div className="">
+          <h1 className="text-red-400">Kocak gaming</h1>
+        </div>
+      </BrowserRouter>
+    </FinanceProvider>
   )
 }
 
