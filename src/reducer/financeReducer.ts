e626f7b5d5a118d96be1f3d633de4e41152bfe56
diqimusyaffa29 <diqimusyaffa4@gmail.com>
2026-04-12 11:00:51 +0800

    feat: :sparkles: Added addBudget, editBudget and deleteBudget reducer

--- src/reducer/financeReducer.ts
+++ src/reducer/financeReducer.ts
@@ -4,7 +4,7 @@ const now = new Date()
 
 export const initialState: FinanceState = {
     transactions: [],
-    budgets:[],
+    budgets: [],
     filter: {
         year: now.getFullYear(),
         month: now.getMonth(),
@@ -12,28 +12,43 @@ export const initialState: FinanceState = {
     },
 }
 
-export function financeReducer(state: FinanceState, action: FinanceAction) : FinanceState{
+export function financeReducer(state: FinanceState, action: FinanceAction): FinanceState {
     switch (action.type) {
-        case "ADD_TRANSACTION":    
+        case "ADD_TRANSACTION":
             return {
                 ...state,
                 transactions: [action.payload, ...state.transactions]
             }
-        case "DELETE_TRANSACTION":    
+        case "DELETE_TRANSACTION":
             return {
                 ...state,
                 transactions: state.transactions.filter(t => t.id !== action.payload)
             }
 
-        case "EDIT_TRANSACTION":    
+        case "EDIT_TRANSACTION":
             return {
                 ...state,
                 transactions: state.transactions.map(t => t.id === action.payload.id ? action.payload : t)
             }
-        case "SET_FILTER":   
+        case "SET_FILTER":
             return {
                 ...state,
-                filter: {...state.filter, ...action.payload}
+                filter: { ...state.filter, ...action.payload }
+            }
+        case "ADD_BUDGET":
+            return {
+                ...state,
+                budgets: [...state.budgets, action.payload]
+            }
+        case "EDIT_BUDGET":
+            return {
+                ...state,
+                budgets: state.budgets.map(b => b.id === action.payload.id ? action.payload : b)
+            }
+        case "DELETE_BUDGET":
+            return {
+                ...state,
+                budgets: state.budgets.filter(b => b.id !== action.payload)
             }
         default:
             return state;
