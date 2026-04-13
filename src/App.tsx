63d87c6c597edfc0345d8fc7e1278fa6f19c7355
diqimusyaffa29 <diqimusyaffa4@gmail.com>
2026-04-13 20:43:56 +0800

    feat: :sparkles: Implemented Sidebar Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { BrowserRouter } from "react-router-dom"
 import { FinanceProvider } from "./context/FinanceContext"
+import { Sidebar } from "./components/layout/Sidebar"
+
 
 function App() {
 
   return (
     <FinanceProvider>
       <BrowserRouter>
-        <div className="">
-          <h1 className="text-red-400">Kocak gaming</h1>
+        <div className="flex min-h-screen">
+          <Sidebar />
+          <main className="flex-1 p-6">
+            <h1>KOCAK GAMING </h1>
+          </main>
         </div>
       </BrowserRouter>
     </FinanceProvider>
