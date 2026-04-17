8f276315eb187660e1a517c4573da720cdbd7027
diqimusyaffa29 <diqimusyaffa4@gmail.com>
2026-04-17 21:40:06 +0800

    feat: :sparkles: Implemented SummarySection at Dashboard Page

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,11 +1,16 @@
 import { FilterBar } from "@/components/dashboard/FilterBar"
+import { SummarySection } from "@/components/dashboard/SummarySection"
 
 function Dashboard() {
     return (
-        <div>
-            <h1>Dashboard Page</h1>
-            <FilterBar />
-        </div>
+        <>
+            <div className="p-4">
+                <FilterBar />
+            </div>
+            <div className="bg-gray-100 p-2 h-screen">
+                <SummarySection />
+            </div>
+        </>
     )
 }
 
