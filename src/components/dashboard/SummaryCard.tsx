435d319896e519b9fd74d7d3985ccbf4fac80337
diqimusyaffa29 <diqimusyaffa4@gmail.com>
2026-04-18 17:53:13 +0800

    feat: :sparkles: Added isCurrency for ternary operation for amount

--- src/components/dashboard/SummaryCard.tsx
+++ src/components/dashboard/SummaryCard.tsx
@@ -4,19 +4,25 @@ type SummaryCardProps = {
     title: string
     amount: number
     description?: string
+    isCurrency?: boolean
 }
 
 
-const SummaryCard = ({ title, amount, description }: SummaryCardProps) => {
+const SummaryCard = ({ title, amount, description, isCurrency }: SummaryCardProps) => {
     return (
-        <div className="rounded-2xl border bg-white p-5 w-full shadow-sm">
+        <div className="rounded-2xl border bg-white p-5 w-full shadow-sm text-center">
             <p className="text-sm text-muted-foreground">
                 {toTitleCase(title)}
             </p>
-
-            <h3 className="mt-2 text-2xl font-bold">
-                Rp {amount.toLocaleString("id-ID")}
-            </h3>
+            {isCurrency ? (
+                <h3 className="mt-2 text-2xl font-bold">
+                    Rp {amount.toLocaleString("id-ID")}
+                </h3>
+            ) : (
+                <h3 className="mt-2 text-2xl font-bold">
+                    {amount.toLocaleString("id-ID")}
+                </h3>
+            )}
 
             {description && (
                 <p className="mt-1 text-xs text-muted-foreground">
