0a2869db7c11cc0a4668cbcff03546ae37bbc8ac
diqimusyaffa29 <diqimusyaffa4@gmail.com>
2026-04-18 17:53:40 +0800

    feat: :sparkles: Added isCurrency value and informative description

--- src/components/dashboard/SummarySection.tsx
+++ src/components/dashboard/SummarySection.tsx
@@ -1,35 +1,46 @@
 import { useTransaction } from "@/hooks/useTransaction"
 import { SummaryCard, SummaryCardProps } from "./SummaryCard"
+import { useFilter } from "@/hooks/useFilter"
+import { MONTHS } from "./FilterBar"
+import { toTitleCase } from "@/utils/toTitleCase"
 
 
 
 const SummarySection = () => {
     const { transactions, summary } = useTransaction()
+    const { filter } = useFilter()
 
     const summaryCardData: SummaryCardProps[] = [
         {
             title: "income",
             amount: summary.income,
+            isCurrency: true,
+            description: `Total in ${MONTHS[filter.month]} on ${filter.category === "all" ? "All categories" : toTitleCase(filter.category)} category`
         },
         {
             title: "expenses",
             amount: summary.expense,
+            isCurrency: true,
+            description: `Total in ${MONTHS[filter.month]} on ${filter.category === "all" ? "All categories" : toTitleCase(filter.category)} category`
         },
         {
             title: "balance",
             amount: summary.balance,
+            isCurrency: true,
+            description: `Total in ${MONTHS[filter.month]} on ${filter.category === "all" ? "All categories" : toTitleCase(filter.category)} category`
         },
         {
             title: "Transactions",
             amount: transactions.length,
-            description: "Total transactions"
+            description: `Total in ${MONTHS[filter.month]} on ${filter.category === "all" ? "All categories" : toTitleCase(filter.category)} category`,
+            isCurrency: false
         },
     ]
 
     return (
         <div className="grid gap-4 md:grid-cols-2 xl:grid-cols-4">
             {summaryCardData.map((item) => (
-                <SummaryCard key={item.title} title={item.title} amount={item.amount} description={item.description} />
+                <SummaryCard isCurrency={item.isCurrency} key={item.title} title={item.title} amount={item.amount} description={item.description} />
             ))}
         </div>
     )
