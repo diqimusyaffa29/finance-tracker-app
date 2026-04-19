0334d59e27fb1bc91923e641ad2b0bb3bbac11e4
diqimusyaffa29 <diqimusyaffa4@gmail.com>
2026-04-19 21:19:02 +0800

    feat: :sparkles: Added ExpenseChart component and implemented to Dashboard page

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,3 +1,4 @@
+import { ExpenseChart } from "@/components/dashboard/ExpenseChart"
 import { FilterBar } from "@/components/dashboard/FilterBar"
 import { SummarySection } from "@/components/dashboard/SummarySection"
 
@@ -7,8 +8,9 @@ function Dashboard() {
             <div className="p-4">
                 <FilterBar />
             </div>
-            <div className="bg-gray-100 p-2 h-screen">
+            <div className="bg-gray-100 p-2 h-screen space-y-5">
                 <SummarySection />
+                <ExpenseChart />
             </div>
         </>
     )
